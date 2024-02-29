998adb9b4ddff1aa52c7febd1aa93efd2fa76c3f
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-02-29 16:52:01 +0900

    fix: 使わない関数を削除しました

--- test/rich_text_converter_test.dart
+++ test/rich_text_converter_test.dart
@@ -1,8 +1,5 @@
-import 'package:rich_text_converter/rich_text_converter.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
+  test('calculate', () {});
 }
