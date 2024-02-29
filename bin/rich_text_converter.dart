de730766f2a75a66ccc3b625676b7b40be98d575
iseruuuuu <iseki.ryutaro@rkb.bb4u.ne.jp>
2024-02-29 16:50:45 +0900

    add: 出力先のファイルを追加しました

---
+++ bin/rich_text_converter.dart
@@ -0,0 +1,7 @@
+import 'package:rich_text_converter/rich_text_converter.dart'
+    as rich_text_converter;
+
+void main(List<String> arguments) {
+  final text = rich_text_converter.getSimpleText();
+  print(text);
+}
